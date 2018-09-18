b14815972358fbce7361b6cd9fe1f6575372088c
Amal Ahmed <37952096+HOPES98@users.noreply.github.com>
2018-09-18 23:01:18 +0300

    Add files via upload

--- KAUgradebookIO.java
+++ KAUgradebookIO.java
@@ -14,6 +14,7 @@ package kaugradebookio;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class KAUgradebookIO {
@@ -71,7 +72,7 @@ public class KAUgradebookIO {
     }
 
     public static void addRecord(Scanner input, PrintWriter output, Student[] students) {
-        char letterGrade = 'A';
+        
 //1 . Scan from file all required information
         int id = Integer.parseInt(input.next());
         String f = input.next();
@@ -83,17 +84,7 @@ public class KAUgradebookIO {
         double fGrade = (one * 0.3) + (two * 0.3) + (three * 0.4);
 
 //3 . determine the lettetr grade
-        if (fGrade > +90) {
-            letterGrade = 'A';
-        } else if ((fGrade < 90) && (fGrade >= 80)) {
-            letterGrade = 'B';
-        } else if ((fGrade < 80) && (fGrade >= 70)) {
-            letterGrade = 'C';
-        } else if ((fGrade < 70) && (fGrade >= 60)) {
-            letterGrade = 'D';
-        } else {
-            letterGrade = 'F';
-        }
+char letterGrade = getLetterGrade(fGrade);        
 
 //4 . Save all information into a new Student object
         Student newStudent = new Student(id, f, l, fGrade, letterGrade);
@@ -123,10 +114,94 @@ public class KAUgradebookIO {
         Student.increaseStudents();
 
         //print the info on the output file:
-        output.print("Command: ADDRECORD\n"
+        output.println("Command: ADDRECORD\n"
                 + students[index].getfName() + " " + students[index].getlName() + " (ID# " + students[index].getiD() + " )"
                 + " has been added to the KAU Grade Book.\n \t his final grade is " + students[index].getFinalGrade() + ""
                 + " ( " + students[index].getLetterGrade() + ")");
 
     }
+    public static void searchStudentsByID(Scanner input, PrintWriter output, Student[] students){
+    //1 . scan from file ID of the student to search for
+        int id = Integer.parseInt(input.next());
+        int search = -1;
+    
+    //2 . search for this student ID using binary search:
+            
+             search = Arrays.binarySearch(students, id);
+        
+        
+    //3 . if the student id is not found, an error message will print
+        if(search==0){
+        output.println("ERROR: there is no record for student ID # "+id+".");
+        }
+      
+    //4. If the student ID is found, the student’s record will print
+        if(search>1){
+        output.println("Command: SEARCHBYID");
+        output.println("Student Record for Faisal Ashihiri (ID # 111):\n" +
+                     "     Exam 1:       "+students[search].getExamGrades(0)+"\n" +
+                     "     Exam 2:       "+students[search].getExamGrades(1)+"\n" +
+                     "     Final Exam:   "+students[search].getExamGrades(2)+"\n" +
+                     "     Final Grade:  "+students[search].getFinalGrade()+"\n" +
+                     "     Letter Grade: "+students[search].getLetterGrade());
+        }
+    
+    }
+    
+    public static void searchStudentsByName(Scanner input, PrintWriter output, Student[] students){
+    //1. Scan from file the first and last name of the student to search for.
+        String fName = input.next();
+        String lName = input.next();
+        
+    //2. Search for this student using first and last name(linear search)
+         int index = 0;
+         for (int i = 0; i < Student.getNumStudents()-1; i++) {
+             String fSearch = students[i].getfName();
+             String lSearch = students[i].getlName();
+             if((fSearch.equalsIgnoreCase(fName))&&(lSearch.equalsIgnoreCase(lName))){
+                 index = i;
+                 break;
+             }
+            
+        }
+    //3. if the student is not found, and error message will print.
+         output.println("ERROR: there is no record for student \""+fName+" "+lName+"\".");
+         
+    //4. if the student is found, the student record will print.
+         output.println("Command: SEARCHBYNAME");
+         output.println("Student Record for Faisal Ashihiri (ID # 111):\n" +
+                     "     Exam 1:       "+students[index].getExamGrades(0)+"\n" +
+                     "     Exam 2:       "+students[index].getExamGrades(1)+"\n" +
+                     "     Final Exam:   "+students[index].getExamGrades(2)+"\n" +
+                     "     Final Grade:  "+students[index].getFinalGrade()+"\n" +
+                     "     Letter Grade: "+students[index].getLetterGrade());
+    }
+    
+    public static void displayStatistics(Scanner input, PrintWriter output, Student[] students){
+    //1. display statistical results for the class
+    }
+    
+    public static void displayAllStudenets(Scanner input, PrintWriter output, Student[] students){
+    //1. display the record of all students along with their grade
+    //2. if there is no student, an error message should print
+    //3. else all students should be printed
+    
+    }
+    
+    public static char getLetterGrade(double fGrade){
+    //1. determin the letter grade of the student from their final, numerical grade(Standard Scale) then return letter Grade
+        if (fGrade > +90) {
+            return 'A';
+        } else if ((fGrade < 90) && (fGrade >= 80)) {
+            return 'B';
+        } else if ((fGrade < 80) && (fGrade >= 70)) {
+            return 'C';
+        } else if ((fGrade < 70) && (fGrade >= 60)) {
+           return 'D';
+        } else {
+            return 'F';
+        }
+        
+    }
+    
 }
